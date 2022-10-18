f0392e4ebfba67e51c91041f68deaf180c368d79
michael <mikeutley@gmail.com>
2022-10-17 20:17:40 -0700

    first tweak of laout

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,20 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#define MIRYOKU_LAYER_BASE \
+&kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
+&hm LGUI A,        &hm LALT R,        &hm LCTRL S,       &hm LSHFT T,       &kp G,             &kp M,             &hm LSHFT N,       &hm LCTRL E,       &hm LALT I,        &hm LGUI O,        \
+&lt U_BUTTON Z,    &hm RALT X,        &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         &hm RALT DOT,      &lt U_BUTTON SLASH,\
+U_NP,              U_NP,              &kp ESC,           &lt U_SYM SPC,     &lt U_NUM RET,     &kp LSHIFT,        &lt U_NAV BSPC,    &kp LGUI,          U_NP,              U_NP
+
+#define MIRYOKU_LAYER_NUM \
+&u_bootloader,     &to U_TAP,         &to U_EXTRA,       &to U_BASE,        U_NA,              &kp LBKT,          &kp NUM_7,         &kp NUM_8,         &kp NUM_9,         &kp RBKT,          \
+&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              &kp EQL,           &kp NUM_4,         &kp NUM_5,         &kp NUM_6,         &kp SEMI,          \
+U_NA,              &kp RALT,          &to U_NAV,         &to U_NUM,         U_NA,              &kp NUM_0,         &kp NUM_1,         &kp NUM_2,         &kp NUM_3,         &kp GRAVE,         \
+U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp MINUS,         &kp NUM_0,         &kp DOT,           U_NP,              U_NP
+
+#define MIRYOKU_LAYER_SYM \
+&kp TAB,           U_NA,              U_NA,              &to U_BASE,        &kp GRAVE,         &kp LBRC,          &kp AMPS,          &kp ASTRK,         &kp LPAR,          &kp RBRC,          \
+&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         &kp EQL,           &kp PLUS,          &kp DLLR,          &kp PRCT,          &kp CRRT,          &kp COLON,         \
+&u_bootloader,     &kp RALT,          &to U_MOUSE,       &kp LBKT,          &kp RBKT,          &kp PIPE,          &kp EXCL,          &kp AT,            &kp HASH,          &kp TILDE,         \
+U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp UNDER,         &kp LPAR,          &kp RPAR,          U_NP,              U_NP
