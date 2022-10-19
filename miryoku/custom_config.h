aa2309da49f115606b1c8237b456f19367332728
michael <mikeutley@gmail.com>
2022-10-18 20:15:51 -0700

    dialing it in

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -2,19 +2,19 @@
 // https://github.com/manna-harbour/miryoku
 
 #define MIRYOKU_LAYER_BASE \
-&kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
-&hm LGUI A,        &hm LALT R,        &hm LCTRL S,       &hm LSHFT T,       &kp G,             &kp M,             &hm LSHFT N,       &hm LCTRL E,       &hm LALT I,        &hm LGUI O,        \
-&lt U_BUTTON Z,    &hm RALT X,        &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         &hm RALT DOT,      &lt U_BUTTON SLASH,\
-U_NP,              U_NP,              &kp ESC,           &lt U_SYM SPC,     &lt U_NUM RET,     &kp LSHIFT,        &lt U_NAV BSPC,    &kp LGUI,          U_NP,              U_NP
+&hm TAB Q,         &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
+&hm ESC A,         &kp R,             &kp S,             &kp T,             &kp G,             &kp M,             &kp N,             &kp E,             &kp I,             &kp O,        \
+&lt U_BUTTON Z,    &hm RALT X,        &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         &hm RALT DOT,      &hm LALT SLASH,\
+U_NP,              U_NP,              &kp LGUI,          &lt U_SYM SPC,     &lt U_NAV RET,     &kp LSHIFT,        &lt U_NUM BSPC,    &kp LCTRL,         U_NP,              U_NP
 
 #define MIRYOKU_LAYER_NUM \
-&u_bootloader,     &to U_TAP,         &to U_EXTRA,       &to U_BASE,        U_NA,              &kp LBKT,          &kp NUM_7,         &kp NUM_8,         &kp NUM_9,         &kp RBKT,          \
-&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              &kp EQL,           &kp NUM_4,         &kp NUM_5,         &kp NUM_6,         &kp SEMI,          \
-U_NA,              &kp RALT,          &to U_NAV,         &to U_NUM,         U_NA,              &kp NUM_0,         &kp NUM_1,         &kp NUM_2,         &kp NUM_3,         &kp GRAVE,         \
+&kp LALT,          &kp LCTRL,         &kp LGUI,          &kp LSHIFT,        U_NA,              &kp PG_UP,         &kp HOME,          &kp UP,            &kp END,           &kp DEL,          \
+&kp N1,            &kp N2,            &kp N3,            &kp N4,            &kp N5,            &kp PG_DN,         &kp LEFT,          &kp DOWN,          &kp RIGHT,         &kp RET,          \
+&kp N6,              &kp N7,          &kp N8,            &kp N9,            &kp N0,            &kp ESC,           &kp NUM_1,         &kp NUM_2,         &kp NUM_3,         &kp GRAVE,         \
 U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp MINUS,         &kp NUM_0,         &kp DOT,           U_NP,              U_NP
 
 #define MIRYOKU_LAYER_SYM \
-&kp TAB,           U_NA,              U_NA,              &to U_BASE,        &kp GRAVE,         &kp LBRC,          &kp AMPS,          &kp ASTRK,         &kp LPAR,          &kp RBRC,          \
-&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         &kp EQL,           &kp PLUS,          &kp DLLR,          &kp PRCT,          &kp CRRT,          &kp COLON,         \
-&u_bootloader,     &kp RALT,          &to U_MOUSE,       &kp LBKT,          &kp RBKT,          &kp PIPE,          &kp EXCL,          &kp AT,            &kp HASH,          &kp TILDE,         \
+&kp SQT,           &kp DQT,           &kp CARET,         &kp QUESTION,      &kp GRAVE,         &kp LBKT,          &kp LESS_THAN,     &kp EQUAL,         &kp GREATER_THAN,  &kp RBKT,          \
+&kp EXCLAMATION,   &kp AT,            &kp HASH,          &kp DOLLAR,        &kp PERCENT,       &kp LBRC,          &kp LPAR,          &kp COLON,         &kp RPAR,          &kp RBRC,         \
+&kp BACKSLASH,     &kp TILDE,         &kp PIPE,          &kp AMPS,          &kp SEMICOLON,     &kp SLASH,         &kp ASTERISK,      &kp MINUS,         &kp PLUS,          &kp UNDER,         \
 U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp UNDER,         &kp LPAR,          &kp RPAR,          U_NP,              U_NP
