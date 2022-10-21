dbe5fa5df856bf193347261e96de7194c6fb9911
michael <mikeutley@gmail.com>
2022-10-20 18:58:57 -0700

    hopefully last change for a while

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -4,20 +4,20 @@
 #define MIRYOKU_LAYER_BASE \
 &hm TAB Q,         &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
 &hm ESC A,         &kp R,             &kp S,             &kp T,             &kp G,             &kp M,             &kp N,             &kp E,             &kp I,             &kp O,        \
-&lt U_BUTTON Z,    &hm RALT X,        &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         &hm RALT DOT,      &hm LALT SLASH,\
-U_NP,              U_NP,              &kp LGUI,          &lt U_SYM SPC,     &lt U_MEDIA RET,   &kp LSHIFT,        &lt U_NUM BSPC,    &kp LCTRL,         U_NP,              U_NP
+&kp Z,             &kp X,             &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         &kp DOT,           &kp SLASH,\
+U_NP,              U_NP,              &mo U_NUM,         &kp SPC,           &kp RET,           &kp LSHIFT,        &kp BSPC,          &mo U_SYM,         U_NP,              U_NP
 
 #define MIRYOKU_LAYER_NUM \
-&kp LALT,          &kp LCTRL,         &kp LGUI,          &kp LSHIFT,        U_NA,              &kp PG_UP,         &kp HOME,          &kp UP,            &kp END,           &kp DEL,          \
-&kp N1,            &kp N2,            &kp N3,            &kp N4,            &kp N5,            &kp PG_DN,         &kp LEFT,          &kp DOWN,          &kp RIGHT,         &kp RET,          \
-&kp N6,            &kp N7,            &kp N8,            &kp N9,            &kp N0,            &kp ESC,           &kp NUM_1,         &kp NUM_2,         &kp NUM_3,         &kp GRAVE,         \
-U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp MINUS,         &kp NUM_0,         &kp DOT,           U_NP,              U_NP
+U_UND,             U_RDO,             U_CUT,             U_CPY,             U_PST,             &kp PG_UP,         &kp HOME,          &kp UP,            &kp END,           &kp DEL,          \
+&kp N1,            &kp N2,            &kp N3,            &kp N4,            &kp N5,            &kp PG_DN,         &kp LEFT,          &kp DOWN,          &kp RIGHT,         &kp ESC,          \
+&kp N6,            &kp N7,            &kp N8,            &kp N9,            &kp N0,            U_NA,              U_NA,              U_NA,              U_NA,              U_NA,         \
+U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              U_NP,              U_NP
 
 #define MIRYOKU_LAYER_SYM \
-&kp SQT,           &kp DQT,           &kp CARET,         &kp QUESTION,      &kp GRAVE,         &kp LBKT,          &kp LESS_THAN,     &kp EQUAL,         &kp GREATER_THAN,  &kp RBKT,          \
-&kp EXCLAMATION,   &kp AT,            &kp HASH,          &kp DOLLAR,        &kp PERCENT,       &kp LBRC,          &kp LPAR,          &kp COLON,         &kp RPAR,          &kp RBRC,         \
-&kp BACKSLASH,     &kp TILDE,         &kp PIPE,          &kp AMPS,          &kp SEMICOLON,     &kp SLASH,         &kp ASTERISK,      &kp MINUS,         &kp PLUS,          &kp UNDER,         \
-U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp UNDER,         &kp LPAR,          &kp RPAR,          U_NP,              U_NP
+&kp EXCLAMATION,   &kp AT,            &kp HASH,          &kp DOLLAR,        &kp PERCENT,       &kp CARET,         &kp AMPS,          &kp ASTERISK,      &kp UNDER,         &kp SEMICOLON,          \
+&kp GRAVE,         &kp TILDE,         &kp LBRC,          &kp LPAR,          &kp LBKT,          &kp COLON,         &sk LGUI,          &sk LALT,          &sk LCTRL,         &SK LSHIFT,         \
+&kp LESS_THAN,     &kp GREATER_THAN,  &kp RBRC,          &kp RPAR,          &kp RBKT,          &kp PIPE,          &kp MINUS,         &kp EQUAL,         &kp PLUS,          &kp BACKSLASH,         \
+U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              U_NP,              U_NP
 
 #define MIRYOKU_LAYER_MEDIA \
 &u_bootloader,     &to U_TAP,         &to U_EXTRA,       &to U_BASE,        U_NA,              &u_rgb_tog,        &u_rgb_eff,        &u_rgb_hui,        &u_rgb_sai,        &u_rgb_bri,        \
